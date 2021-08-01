b9d648be6b6eb0c53018cd3720577ff0ae69ca8a
furbox <furbox@gmail.com>
2021-08-01 16:35:49 -0500

    socket

--- socket-server/models/sockets.js
+++ socket-server/models/sockets.js
@@ -9,6 +9,15 @@ class Sockets {
             console.log('[SOCKET]: Client Connected');
             console.log('[SOCKET]: ', socket.id);
 
+            //validar jwt
+            //desconectar si el token no es valido
+
+            //saber que usuario esta activo mediante uid
+
+            //emitir a todos los usuarios conectados
+
+            //socket join
+
             socket.emit('[SERVER]:msg-bienvenida', {
                 msg: 'Bienvenido al Server',
                 fecha: new Date()
