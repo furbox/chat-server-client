5e2236522e1ad9ba959de318d7614c8626bd604b
furbox <furbox@gmail.com>
2021-08-06 15:37:26 -0500

    socket chat send msg

--- chat-app/src/component/SendMessage.jsx
+++ chat-app/src/component/SendMessage.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const SendMessage = () => {
+    const [mensaje, setMensaje] = useState('');
+
+    const onChange = ({ target }) => {
+        setMensaje(target.value);
+    }
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        if(mensaje.length === 0) {return;}
+        setMensaje('');
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="type_msg row">
                 <div className="input_msg_write col-sm-9">
-                    <input type="text" className="write_msg" placeholder="Mensaje..." />
+                    <input onChange={onChange} value={mensaje} type="text" className="write_msg" placeholder="Mensaje..." />
                 </div>
                 <div className="col-sm-3 text-center">
                     <button className="msg_send_btn mt-3" type="submit">
