f4c93fc48818f0058a0dbcb09657ce3caa80991c
furbox <furbox@gmail.com>
2021-08-06 19:48:26 -0500

    socket chat moment

--- chat-app/src/ChatApp.jsx
+++ chat-app/src/ChatApp.jsx
@@ -3,6 +3,9 @@ import { AuthProvider } from './auth/AuthContext';
 import { ChatProvider } from './context/chat/ChatContext';
 import { SocketProvider } from './context/SocketContext';
 import { AppRouter } from './router/AppRouter';
+import moment from 'moment';
+import 'moment/locale/es';
+moment.locale('es');
 
 export const ChatApp = () => {
     return (
